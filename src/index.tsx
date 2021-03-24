6dca29a882b5b25e2db4aa6fbc6581ae6aa7f990
Dino Tudor <dinotudor@gmail.com>
2021-03-24 09:57:24 -0300

    Mirage JS + axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.timing = 2000;
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Freelance',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
